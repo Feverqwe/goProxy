acf13571ee14f8fa87efe2522246f5f520d890f7
Anton Vikulov <rndnm@yandex-team.ru>
2026-01-29 10:45:55 +0300

    fix create config

--- config/config.go
+++ config/config.go
@@ -58,20 +58,29 @@ func LoadConfig(configPath string) (*ProxyConfig, error) {
 			"socks5": "socks5://localhost:1080",
 			"http":   "http://localhost:8081",
 			"direct": "",
+			"block":  "#",
 		},
 		ListenAddr: ":8080",
 		LogLevel:   "info",
 		Rules: []RuleConfig{
 			{
 				Proxy: "direct",
-				Ips:   "192.168.1.0/24 10.0.0.0/8",
-				Hosts: "localhost* *.local *.example.com",
+				Ips:   "192.168.1.0/24 10.0.0.0/8 172.16.0.0/12",
+				Hosts: "localhost *.local *.example.com internal.company.com",
 				URLs:  "http://internal-api.company.com/v1/* https://*.internal.com/api/*",
 			},
 			{
 				Proxy: "socks5",
 				Not:   true,
 				Hosts: "*.google.com *.youtube.com",
+			},
+			{
+				Proxy: "http",
+				Hosts: "*.external.com api.*.com",
+			},
+			{
+				Proxy: "block",
+				Hosts: "*.malicious.com *.spam.com",
 			},
 		},
 		IPCacheSize: 1000,
@@ -86,6 +95,10 @@ func LoadConfig(configPath string) (*ProxyConfig, error) {
 				return nil, fmt.Errorf("error migrating config from JSON to YAML: %v", err)
 			}
 		} else {
+			// Create default config file
+			if err := saveDefaultConfig(configPath, config); err != nil {
+				return nil, fmt.Errorf("error creating default config file: %v", err)
+			}
 			return config, nil
 		}
 	}
@@ -101,6 +114,23 @@ func LoadConfig(configPath string) (*ProxyConfig, error) {
 	}
 
 	return config, nil
+}
+
+// saveDefaultConfig сохраняет конфигурацию по умолчанию в файл
+func saveDefaultConfig(configPath string, config *ProxyConfig) error {
+	file, err := os.Create(configPath)
+	if err != nil {
+		return fmt.Errorf("error creating config file: %v", err)
+	}
+	defer file.Close()
+
+	encoder := yaml.NewEncoder(file)
+	encoder.SetIndent(2)
+	if err := encoder.Encode(config); err != nil {
+		return fmt.Errorf("error encoding config to YAML: %v", err)
+	}
+
+	return nil
 }
 
 // ParseStringToList parses a string with comma or space separated values into a slice
