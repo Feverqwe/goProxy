ad2a8197a39e79b1f82564b78dd5636c5377cdd6
Anton Vikulov <rndnm@yandex-team.ru>
2026-01-29 11:13:43 +0300

    tray icon for win

--- tray/tray.go
+++ tray/tray.go
@@ -1,59 +1,108 @@
-//go:build !darwin
+//go:build !linux
 
 package tray
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
+	"goProxy/assets"
+
+	"github.com/getlantern/systray"
 )
 
-// TrayManager is a stub implementation for non-macOS systems
+// TrayManager manages the system tray on all platforms except Linux
 type TrayManager struct {
 	quitChan       chan struct{}
 	reloadChan     chan struct{}
 	openConfigChan chan struct{}
 }
 
+// NewTrayManager creates a new tray manager for all platforms except Linux
 func NewTrayManager() *TrayManager {
-	tm := &TrayManager{
-		quitChan:       make(chan struct{}),
-		reloadChan:     make(chan struct{}),
-		openConfigChan: make(chan struct{}),
+	return &TrayManager{
+		quitChan:       make(chan struct{}, 1),
+		reloadChan:     make(chan struct{}, 1),
+		openConfigChan: make(chan struct{}, 1),
 	}
-
-	// Set up signal handling to close quitChan on interrupt
-	go func() {
-		sigChan := make(chan os.Signal, 1)
-		signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
-		<-sigChan
-		close(tm.quitChan)
-	}()
-
-	return tm
 }
 
+// Start initializes and starts the enhanced system tray
 func (tm *TrayManager) Start() {
-	// No-op for non-macOS
+	systray.Run(tm.onReady, tm.onExit)
 }
 
+// WaitForQuit waits for the quit signal
 func (tm *TrayManager) WaitForQuit() {
-	// Wait for quit signal
 	<-tm.quitChan
 }
 
+// GetQuitChan returns the quit channel for external monitoring
 func (tm *TrayManager) GetQuitChan() <-chan struct{} {
 	return tm.quitChan
 }
 
+// GetReloadChan returns the reload config channel
 func (tm *TrayManager) GetReloadChan() <-chan struct{} {
 	return tm.reloadChan
 }
 
+// GetOpenConfigChan returns the open config directory channel
 func (tm *TrayManager) GetOpenConfigChan() <-chan struct{} {
 	return tm.openConfigChan
 }
 
+// onReady is called when systray is ready
+func (tm *TrayManager) onReady() {
+	systray.SetTemplateIcon(assets.IconSVGData, assets.IconSVGData)
+	systray.SetTooltip("GoProxy - HTTP Proxy Server")
+
+	tm.createMenu()
+}
+
 func (tm *TrayManager) Exit() {
-	// No-op for non-macOS
+	systray.Quit()
+}
+
+// onExit is called when systray exits
+func (tm *TrayManager) onExit() {
+	close(tm.quitChan)
+}
+
+// createMenu creates the enhanced tray menu with config options
+func (tm *TrayManager) createMenu() {
+	// Add separator and config options
+	systray.AddSeparator()
+	reloadItem := systray.AddMenuItem("Reload config", "Reload configuration file")
+	openConfigItem := systray.AddMenuItem("Open config directory", "Open directory containing config file")
+	systray.AddSeparator()
+
+	// Add the exit button
+	quitItem := systray.AddMenuItem("Quit", "Close app")
+
+	// Handle each menu item in separate goroutines
+	go func() {
+		for range reloadItem.ClickedCh {
+			// Signal to reload configuration
+			select {
+			case tm.reloadChan <- struct{}{}:
+			default:
+				// Channel is full, ignore
+			}
+		}
+	}()
+
+	go func() {
+		for range openConfigItem.ClickedCh {
+			// Signal to open config directory
+			select {
+			case tm.openConfigChan <- struct{}{}:
+			default:
+				// Channel is full, ignore
+			}
+		}
+	}()
+
+	go func() {
+		for range quitItem.ClickedCh {
+			systray.Quit()
+		}
+	}()
 }
